36fe60303b0d14785906839d88fab2b839d2c8b2
Jan Mahbub Milon <jan.milon.56@gmail.com>
2025-02-01 01:25:20 +0600

    Add car class with properties and update main function to create a car instance

--- bin/dart_oop.dart
+++ bin/dart_oop.dart
@@ -1,5 +1,9 @@
+import 'package:dart_oop/car.dart';
 import 'package:dart_oop/dart_oop.dart' as dart_oop;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_oop.calculate()}!');
+  final mycar =
+      car(make: 'BMW', model: 'A7', year: 2025, color: 'Red', price: 1.5);
+
+  print(mycar.make);
 }
--- lib/car.dart
+++ lib/car.dart
@@ -1,1 +1,14 @@
-class car {}
+class car {
+  String make;
+  String model;
+  int year;
+  String color;
+  double price;
+
+  car(
+      {required this.make,
+      required this.model,
+      required this.year,
+      required this.color,
+      required this.price});
+}
